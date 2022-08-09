b5048fd072f03389522e8dcc50c19ed3614787e1
Suren <dsuren1@gmail.com>
2022-08-09 19:37:07 +0530

    Lint and unit test fix

--- js/plugins.js
+++ js/plugins.js
@@ -5,10 +5,10 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import productPlugins from '@mapstore/product/plugins'
+import productPlugins from '@mapstore/product/plugins';
 
-import DateFilter from '@js/plugins/DateFilter'
-import IdentifySettingsPlugin from '@js/plugins/IdentifySettings'
+import DateFilter from '@js/plugins/DateFilter';
+import IdentifySettingsPlugin from '@js/plugins/IdentifySettings';
 
 export default {
     requires: {
